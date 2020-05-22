18b8df13e66c2d82fc6283403e312eecca5ca491
Carlos Pareja <saulroc@hotmail.com>
2020-05-22 13:40:27 +0200

    unit test

--- src/app/Model/jugador.spec.ts
+++ src/app/Model/jugador.spec.ts
@@ -1,5 +1,6 @@
 import { Jugador } from './jugador';
 import { Ficha } from './ficha';
+import { Pueblo } from './pueblo';
 
 describe('agregarMina', () => {
     let jugador: Jugador;
@@ -59,5 +60,116 @@ describe('quitarMina', () => {
         expect(jugador.minas).not.toContain(mina2);
         expect(jugador.minas).toContain(mina);        
         expect(jugador.minas.length).toBe(1);
+    });
+});
+
+
+describe('agregarPueblo', () => {
+    let jugador: Jugador;
+    let pueblo: Pueblo;
+    let pueblo2: Pueblo;
+
+    beforeEach(() => {
+        jugador = new Jugador('Paco', '#000000', 0, false);
+        pueblo = new Pueblo('#000000');
+        pueblo2 = new Pueblo('#000000');
+
+    });
+
+    it('Agregar un pueblo a un jugador debe aumentar los pueblos del jugador, los puntos y el numero de pelotones que puede mover', () =>{
+        spyOn(pueblo, 'getPuntos').and.callFake(() => {
+            return 1;
+        })
+        jugador.agregarPueblo(pueblo);
+
+        expect(jugador.pueblos).toContain(pueblo);
+        expect(jugador.pueblos.length).toBe(1);
+        expect(jugador.maximoPelotonesMoviendo).toBe(1);
+        expect(jugador.puntos).toBe(1);
+
+    });
+
+    it('Agregar un pueblo que ya existe debe dejar al jugador igual', () =>{        
+        spyOn(pueblo, 'getPuntos').and.callFake(() => {
+            return 1;
+        })
+        jugador.agregarPueblo(pueblo);
+        jugador.agregarPueblo(pueblo);
+
+        expect(jugador.pueblos).toContain(pueblo);
+        expect(jugador.pueblos.length).toBe(1);
+        expect(jugador.maximoPelotonesMoviendo).toBe(1);
+        expect(jugador.puntos).toBe(1);
+
+    });
+});
+
+describe('quitarPueblo', () => {
+    let jugador: Jugador;
+    let pueblo: Pueblo;
+    let pueblo2: Pueblo;
+
+    beforeEach(() => {
+        jugador = new Jugador('Paco', '#000000', 0, false);
+        pueblo = new Pueblo('#000000');
+        pueblo2 = new Pueblo('#000000');
+
+    });
+
+    it('Quitar un pueblo a un jugador debe reducir los pueblos del jugador, los puntos y el numero de pelotones que puede mover', () =>{
+        spyOn(pueblo, 'getPuntos').and.callFake(() => {
+            return 1;
+        })
+
+        jugador.agregarPueblo(pueblo);
+        jugador.quitarPueblo(pueblo);
+
+        expect(jugador.pueblos).not.toContain(pueblo);
+        expect(jugador.pueblos.length).toBe(0);
+        expect(jugador.maximoPelotonesMoviendo).toBe(0);
+        expect(jugador.puntos).toBe(0);
+
+    });
+
+    it('Quitar un pueblo que ya no existe debe dejar al jugador igual', () =>{        
+        let spy = spyOn(pueblo, 'getPuntos').and.returnValue(1);
+
+        jugador.agregarPueblo(pueblo);
+        jugador.quitarPueblo(pueblo2);
+
+        expect(jugador.pueblos).toContain(pueblo);
+        expect(jugador.pueblos.length).toBe(1);
+        expect(jugador.maximoPelotonesMoviendo).toBe(1);
+        expect(jugador.puntos).toBe(1);
+        //expect(spy).toHaveBeenCalled();
+
+    });
+});
+
+describe('esPropietario', () => {
+    let jugador: Jugador;
+    let pueblo: Pueblo;
+    let pueblo2: Pueblo;
+
+    beforeEach(() => {
+        jugador = new Jugador('Paco', '#000000', 0, false);
+        pueblo = new Pueblo('#000000');
+        pueblo2 = new Pueblo('#000000');
+        jugador.agregarPueblo(pueblo);
+
+    });
+
+    it('Comprobamos con un pueblo que pertenece al jugador que la funcion indica que es propietario', () =>{
+        let resultado = jugador.esPropietario(pueblo);
+
+        expect(resultado).toBeTruthy();        
+
+    });
+
+    it('Comprobamos con un pueblo que NO pertenece al jugador como la funcion indica que no es propietario', () =>{        
+        let resultado = jugador.esPropietario(pueblo2);
+
+        expect(resultado).not.toBeTruthy();
+
     });
 });
