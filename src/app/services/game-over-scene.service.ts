80683a1c993fa026c9d1ef5dc5c1c65dea3eb97f
Carlos Pareja <saulroc@hotmail.com>
2020-05-22 15:13:48 +0200

    mensaje de puntuacion de jugadores

--- src/app/services/game-over-scene.service.ts
+++ src/app/services/game-over-scene.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import * as Phaser from 'phaser';
 import { Jugador } from '../Model/jugador';
 
+const COLOR_LETRA_BLANCO = '#FFFFFF';
+
 @Injectable({
     providedIn: 'root'
   })
@@ -19,6 +21,24 @@ import { Jugador } from '../Model/jugador';
     }
 
     public create() {
-        var texto = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "GAME OVER!");
+        
+        var bgImage = this.add.rectangle(0, 0, this.cameras.main.width, this.cameras.main.height, 0x000000, 0.5);;
+        bgImage.setOrigin(0, 0);
+
+        var estilo = {
+            font: 'bold 16pt Arial',
+            fill: COLOR_LETRA_BLANCO,
+            align: 'center',
+            wordWrap: true
+        };
+        var texto = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "GAME OVER!", estilo);
+        texto.setOrigin(0.5);
+
+        this.jugadores.forEach(jugador => {
+            
+            texto = this.add.text(this.cameras.main.centerX, texto.y + texto.height, jugador.nombre + ": " + jugador.puntos, estilo);
+            texto.setOrigin(0.5);
+
+        });
     }
   }
