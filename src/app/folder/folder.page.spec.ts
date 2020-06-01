4596a9143878be0f73b0bd976e4a0f95b5c6dc2b
Carlos Pareja <saulroc@hotmail.com>
2020-06-01 21:08:40 +0200

    integration test

--- src/app/folder/folder.page.spec.ts
+++ src/app/folder/folder.page.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { FolderPage } from './folder.page';
@@ -20,5 +21,27 @@ describe('FolderPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Debe renderizar el folder', () => {
+    let folder = 'Inbox'
+    component.folder = folder;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.ion-title'));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain(folder);
+  });
+
+  it('Debe inicializar ion-phaser si está a true inicialize', () => {
+    let folder = 'Inbox'
+    component.folder = folder;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.ion-title'));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain(folder);
   });
 });
