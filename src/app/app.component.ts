4e091a76567eef23d9fce78c8416bc17c148ec91
Carlos Pareja <saulroc@hotmail.com>
2020-08-24 17:44:46 +0200

    lista de folders actualizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,38 +13,17 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Partida',
+      url: '/folder/partida',
+      icon: 'gamepad'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
+      title: 'Nueva Partida',
+      url: '/folder/nuevaPartida',
       icon: 'paper-plane'
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-
+  
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
