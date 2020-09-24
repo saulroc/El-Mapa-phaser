4a572dbda82635eaccd4986e6f9380351cac9082
Carlos Pareja <saulroc@hotmail.com>
2020-09-24 10:24:22 +0200

    agregar jugadores a la partida

--- src/app/folder/nuevaPartida/nueva-partida.ts
+++ src/app/folder/nuevaPartida/nueva-partida.ts
@@ -9,11 +9,12 @@ import { Jugador } from '../../Model/jugador';
   })
 export class NuevaPartida {
     public jugadores: Jugador[];
+    private colores = ["0xff0000", "0x0000ff", "0x008000", "0xffff00", "0xD2B48C", "0xff4500", "0x800080", "0x00ffff"]; 
 
     constructor() { 
         this.jugadores = [];
-        this.jugadores.push(new Jugador("Jugador 1", "", this.jugadores.length + 1, false));
-        this.jugadores.push(new Jugador("Jugador 2", "", this.jugadores.length + 1, false));
+        this.jugadores.push(new Jugador("Jugador 1", this.colores[0], this.jugadores.length + 1, false));
+        this.jugadores.push(new Jugador("Jugador 2", this.colores[1], this.jugadores.length + 1, false));
     }
 
     ngOnInit() {
@@ -21,6 +22,14 @@ export class NuevaPartida {
     }
 
     agregarJugador() {
-        this.jugadores.push(new Jugador("nuevo jugador", "", this.jugadores.length + 1, false));
+        if(this.jugadores.length < this.colores.length)
+            this.jugadores.push(new Jugador("nuevo jugador", this.colores[this.jugadores.length], this.jugadores.length + 1, false));
+    }
+    quitarJugador() {
+        if (this.jugadores.length > 2)
+            this.jugadores.pop();
+    }
+    cambiarCPU(jugador: Jugador) {
+        jugador.CPU = !jugador.CPU;
     }
 }
