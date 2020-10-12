c3e1bdea588e3bdbcf7e3ad7812dcf00732d8043
Carlos Pareja <saulroc@hotmail.com>
2020-10-12 12:07:50 +0200

    redirigir a la pagina de nueva partida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/nuevaPartida',
+    redirectTo: 'nuevaPartida',
     pathMatch: 'full'
   },
   {
